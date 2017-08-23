07364762eed600e08a8d9a064631c14d468ebdd1
Joseph <aelphaeis@live.com>
2017-08-23 15:06:13 -0400

    User Resource.

--- src/main/java/com/crusnikatelier/oauth2/rest/UserResource.java
+++ src/main/java/com/crusnikatelier/oauth2/rest/UserResource.java
@@ -5,5 +5,20 @@ import javax.ws.rs.Path;
 @Path("User")
 public class UserResource {
 	
+	void createUser(){
+		//TODO implement 
+	}
+	
+	void modifyUser(){
+		//TODO implement 
+	}
+	
+	void getUser(){
+		//TODO implement 
+	}
+	
+	void deleteUser(){
+		//TODO implement 
+	}
 
 }
