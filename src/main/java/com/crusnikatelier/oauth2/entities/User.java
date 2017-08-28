6420f57ab0721098eda9c26d4036aaec00eb7487
Joseph Morain <aelphaeis@live.com>
2017-08-28 15:17:12 -0400

    getters and setters user objects.

--- src/main/java/com/crusnikatelier/oauth2/entities/User.java
+++ src/main/java/com/crusnikatelier/oauth2/entities/User.java
@@ -8,4 +8,35 @@ public class User {
 	String salt;
 	String password;
 	Date joined;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSalt() {
+		return salt;
+	}
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getJoined() {
+		return joined;
+	}
+	public void setJoined(Date joined) {
+		this.joined = joined;
+	}
 }
