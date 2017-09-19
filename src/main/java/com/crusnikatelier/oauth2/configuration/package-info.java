5247da6ccbd1484bc03309ef344e2617e434deb0
Joseph Morain <aelphaeis@live.com>
2017-09-19 15:36:50 -0400

    Refactored the init package.

---
+++ src/main/java/com/crusnikatelier/oauth2/configuration/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package is used for classes that will be used in the start up in the configuration of the application.
+ */
+/**
+ * @author Joseph Morain
+ */
+package com.crusnikatelier.oauth2.configuration;
--- src/main/java/com/crusnikatelier/oauth2/init/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package is used for classes that will be used in the start up of the application.
- */
-/**
- * @author Joseph Morain
- */
-package com.crusnikatelier.oauth2.init;
