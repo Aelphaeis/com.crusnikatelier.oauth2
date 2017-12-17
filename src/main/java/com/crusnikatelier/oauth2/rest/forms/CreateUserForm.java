432f1ce6fa53b007739bd5904d333df8df5c6165
Joseph <aelphaeis@live.com>
2017-12-16 20:54:22 -0500

    Additions to user form, change type of exception validate throws.

--- src/main/java/com/crusnikatelier/oauth2/rest/forms/CreateUserForm.java
+++ src/main/java/com/crusnikatelier/oauth2/rest/forms/CreateUserForm.java
@@ -1,5 +1,12 @@
 package com.crusnikatelier.oauth2.rest.forms;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.crusnikatelier.oauth2.exceptions.AggregationException;
 import com.crusnikatelier.oauth2.rest.UserResource;
 
 /**
@@ -8,11 +15,44 @@ import com.crusnikatelier.oauth2.rest.UserResource;
  * @see UserResource#createUser
  * @author Aelphaeis
  */
+@XmlRootElement
 public class CreateUserForm implements Form {
-
+	String email;
+	String password;
+	
 	@Override
-	public void validate() throws ValidationException {
-		// TODO Auto-generated method stub
-		
+	public void validate() throws AggregationException {
+		List<ValidationException> exceptions = new ArrayList<>();
+		if(email == null || email.isEmpty()) {
+			String err = "email cannot be null or blank";
+			exceptions.add(new ValidationException(err));
+		}
+		if(password == null || password.isEmpty()) {
+			String err = "password cannot be null or blank";
+			exceptions.add(new ValidationException(err));
+		}
+		if(!exceptions.isEmpty()) {
+			String err = "an error occurred during validation";
+			throw new AggregationException(err, exceptions);
+		}
 	}
+	
+	@XmlElement
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@XmlElement
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
