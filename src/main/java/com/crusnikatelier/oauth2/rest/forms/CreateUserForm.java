5802d0c66dbe82bb1510af3bb1c78537b1c81a21
Joseph <aelphaeis@live.com>
2017-12-16 20:42:04 -0500

    added create user form

---
+++ src/main/java/com/crusnikatelier/oauth2/rest/forms/CreateUserForm.java
@@ -0,0 +1,18 @@
+package com.crusnikatelier.oauth2.rest.forms;
+
+import com.crusnikatelier.oauth2.rest.UserResource;
+
+/**
+ * This class represents the information necessary to create a user.
+ * 
+ * @see UserResource#createUser
+ * @author Aelphaeis
+ */
+public class CreateUserForm implements Form {
+
+	@Override
+	public void validate() throws ValidationException {
+		// TODO Auto-generated method stub
+		
+	}
+}
