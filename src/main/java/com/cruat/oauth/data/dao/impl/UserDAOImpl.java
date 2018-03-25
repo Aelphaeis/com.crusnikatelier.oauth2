8a038b11828b65e3f6826a40f35a8e50b23a10ae
Joseph <aelphaeis@live.com>
2018-03-25 04:29:16 -0400

    add spring startup listener.

---
+++ src/main/java/com/cruat/oauth/data/dao/impl/UserDAOImpl.java
@@ -0,0 +1,29 @@
+package com.cruat.oauth.data.dao.impl;
+
+import org.springframework.stereotype.Component;
+
+import com.cruat.oauth.data.dao.UserDAO;
+import com.cruat.oauth.data.entities.User;
+
+@Component
+public class UserDAOImpl implements UserDAO {
+
+	@Override
+	public User create(User obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User update(User obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(User obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/com/cruat/oauth/listener/SpringStartupListener.java
@@ -0,0 +1,43 @@
+package com.cruat.oauth.listener;
+
+
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Application Lifecycle Listener implementation class SpringStartupListener
+ *
+ */
+@WebListener
+public class SpringStartupListener implements ServletContextListener {
+	private static final Logger logger = LogManager.getLogger(SpringStartupListener.class);
+	private static final String LOCATION = "context.xml";
+	ConfigurableApplicationContext ctxt;
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+    	
+    	ctxt = new ClassPathXmlApplicationContext(LOCATION);
+    	for(String name : ctxt.getBeanDefinitionNames()) {
+    		logger.info(name);
+    	}
+    }
+	
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+    	ctxt.close();
+    }
+
+}
