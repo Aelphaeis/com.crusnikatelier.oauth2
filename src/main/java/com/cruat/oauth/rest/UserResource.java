d33bcac165dcfc67f47622b92d8ac3be7a6878f9
Joseph <aelphaeis@live.com>
2018-09-26 08:56:25 -0400

    Made UserResource into rest controller.

--- src/main/java/com/cruat/oauth/rest/UserResource.java
+++ src/main/java/com/cruat/oauth/rest/UserResource.java
@@ -5,11 +5,15 @@ import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cruat.oauth.dto.UserDTO;
 import com.cruat.oauth.rest.forms.CreateUserForm;
 import com.cruat.oauth.services.UserService;
 import com.cruat.oauth.utilities.Users;
+
+@RestController
 public class UserResource {
 
 	HttpServletRequest request;
@@ -29,6 +33,7 @@ public class UserResource {
 	public void modifyUser() {
 	}
 
+	@GetMapping("me")
 	public UserDTO getUser() {
 		UserDTO dto = new UserDTO();
 		dto.setJoined(new Date());
