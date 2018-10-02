1fecc6968116553c3e2882587e43ccf27ada3c14
Joseph <aelphaeis@live.com>
2018-10-02 18:19:46 -0400

    formatting.

--- src/test/java/com/cruat/oauth/feature/users/UserControllerTest.java
+++ src/test/java/com/cruat/oauth/feature/users/UserControllerTest.java
@@ -17,39 +17,37 @@ import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserControllerTest {
-	
-//	private static final Logger logger = LogManager.getLogger();
+
+	// private static final Logger logger = LogManager.getLogger();
 	public static final String USER = "user@cruat.com";
 	public static final String PASS = "pswrd";
 	public static final String UUID = "739eae4f-33da-4760-8931-6a8a809c7e24";
 
 	@Mock
 	UserService userService;
-	
+
 	@InjectMocks
 	UserController controller;
-	
+
 	private MockHttpServletRequestBuilder requestBuilder;
 	private StandaloneMockMvcBuilder mvcBuilder;
-	
 
-	
 	@Test
 	public void createUser_user_registered() throws Exception {
-		//build raw mocks
-		//TODO fix test, this should return valid user
+		// build raw mocks
+		// TODO fix test, this should return valid user
 		when(userService.createUser(USER, PASS)).thenReturn(null);
-		
-		//build request
+
+		// build request
 		requestBuilder = MockMvcRequestBuilders.post("/users");
 		requestBuilder.param("email", USER);
 		requestBuilder.param("password", "password");
-		
-		//Build controller
+
+		// Build controller
 		mvcBuilder = MockMvcBuilders.standaloneSetup(controller);
 		MockMvc mvc = mvcBuilder.build();
-		
-		//run test
+
+		// run test
 		ResultActions results = mvc.perform(requestBuilder);
 		results.andExpect(status().isOk());
 	}
