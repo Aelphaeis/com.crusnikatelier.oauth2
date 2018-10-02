39befecf87740ebf52d89ffe9dcb18db3bcc5425
Joseph Morain <morain@echoworx.com>
2018-10-02 14:23:14 -0400

    remove interface for dao.

--- src/main/java/com/cruat/oauth/common/data/dao/UserDAO.java
+++ src/main/java/com/cruat/oauth/common/data/dao/UserDAO.java
@@ -1,8 +1,29 @@
 package com.cruat.oauth.common.data.dao;
 
+import org.springframework.stereotype.Component;
+
 import com.cruat.oauth.common.data.DataAccessObject;
 import com.cruat.oauth.common.data.entities.User;
 
-public interface UserDAO extends DataAccessObject<User> {
-	
+@Component
+public class UserDAO implements DataAccessObject<User> {
+
+	@Override
+	public User create(User obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User update(User obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(User obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
