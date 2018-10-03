a889a666f6ab768dde3a917317fba15b6baed5ba
Joseph <aelphaeis@live.com>
2018-10-02 20:08:19 -0400

    binding Request Body to form

--- src/main/java/com/cruat/oauth/feature/users/UserController.java
+++ src/main/java/com/cruat/oauth/feature/users/UserController.java
@@ -2,6 +2,7 @@ package com.cruat.oauth.feature.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cruat.oauth.common.data.entities.User;
@@ -17,7 +18,7 @@ public class UserController {
 	}
 
 	@PostMapping(path="users")
-	public UserDTO createUser(CreateUserForm form) {
+	public UserDTO createUser(@RequestBody CreateUserForm form) {
 		String email = form.getEmail();
 		String pass = form.getPassword();
 		User user = userService.createUser(email, pass);
