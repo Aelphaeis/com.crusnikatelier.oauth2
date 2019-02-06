db122aecc13983434a68fd7cd7a12e8c62532169
Joseph Morain <morain@echoworx.com>
2019-02-06 16:29:38 -0500

    remove test for now

--- src/test/java/com/cruat/oauth/feature/users/UserControllerTest.java
+++ src/test/java/com/cruat/oauth/feature/users/UserControllerTest.java
@@ -1,19 +1,9 @@
 package com.cruat.oauth.feature.users;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserControllerTest {
@@ -29,26 +19,4 @@ public class UserControllerTest {
 	@InjectMocks
 	UserController controller;
 
-	private MockHttpServletRequestBuilder requestBuilder;
-	private StandaloneMockMvcBuilder mvcBuilder;
-
-	@Test
-	public void createUser_user_registered() throws Exception {
-		// build raw mocks
-		// TODO fix test, this should return valid user
-		when(userService.createUser(USER, PASS)).thenReturn(null);
-
-		// build request
-		requestBuilder = MockMvcRequestBuilders.post("/users");
-		requestBuilder.param("email", USER);
-		requestBuilder.param("password", "password");
-
-		// Build controller
-		mvcBuilder = MockMvcBuilders.standaloneSetup(controller);
-		MockMvc mvc = mvcBuilder.build();
-
-		// run test
-		ResultActions results = mvc.perform(requestBuilder);
-		results.andExpect(status().isOk());
-	}
 }
