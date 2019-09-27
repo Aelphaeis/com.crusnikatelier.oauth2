a4a14c84086949e7915f8e76af029f838e137b77
Joseph Morain <morain@echoworx.com>
2019-09-27 17:31:44 -0400

    update mockito

--- src/test/java/com/cruat/oauth/feature/users/UserControllerTest.java
+++ src/test/java/com/cruat/oauth/feature/users/UserControllerTest.java
@@ -3,7 +3,7 @@ package com.cruat.oauth.feature.users;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserControllerTest {
