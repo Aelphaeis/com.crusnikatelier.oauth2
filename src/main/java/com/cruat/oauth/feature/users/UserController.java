48a116d9354ac31215b5bc3b46ebdd0bfa6f2693
Joseph Morain <morain@echoworx.com>
2019-09-27 17:29:10 -0400

    create ping for testing

--- src/main/java/com/cruat/oauth/feature/users/UserController.java
+++ src/main/java/com/cruat/oauth/feature/users/UserController.java
@@ -1,6 +1,7 @@
 package com.cruat.oauth.feature.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,11 @@ public class UserController {
 		String pass = form.getPassword();
 		User user = userService.createUser(email, pass);
 		return new UserDTO(user);
+	}
+	
+	@GetMapping("ping")
+	public String ping() {
+		return "ping";
 	}
 	
 	protected UserService getUserService() {
